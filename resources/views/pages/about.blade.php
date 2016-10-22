a5c724ec7fc6ca529efc9bdb37a37b4d31fbfe67
Henry Tseng <henrybear327@gmail.com>
2016-10-22 08:29:25 +0800

    Add view works

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: henrybear327
+ * Date: 22/10/2016
+ * Time: 8:26 AM
+ */
+
+echo "Hello there";
--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,22 @@
 |
 */
 
+// handle the request inline
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome'); // resources/views/welcome.blade.php -> welcome
 });
+
+Route::get('/hi', function() {
+    return 'Hi there!';
+});
+
+Route::get('/pagesHi', function() {
+    //return view('pages.about');
+    return view('pages/about');
+});
+
 
 Auth::routes();
 
+// controller -> think of a collection of route closures :)
 Route::get('/home', 'HomeController@index');
