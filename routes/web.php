f77e77216ccf842079aa4be267d68fc11937a0e1
Henry Tseng <henrybear327@gmail.com>
2016-10-22 08:39:07 +0800

    View Data and Blade

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,21 @@ Route::get('/pagesHi', function() {
     return view('pages/about');
 });
 
+Route::get('/passingData', function() {
+    //return view('pages/about'); // dot, slash are both ok
+    
+    $people = ['Taylor', 'Matt', 'Jeffery'];
+    //$people = [];
+
+    // variable passing, at second argument
+    return view('pages.passingData', [
+        'people' => $people]
+    );
+    // return view('pages/about', compact('people'));
+    // return view('pages/about')->with('people', $people);
+    // return view('pages/about')->withPeople($people);
+});
+
 
 Auth::routes();
 
