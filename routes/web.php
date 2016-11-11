cd7654559e086d033dbe71805b8868f7f3c9e2fe
Henry Tseng <henrybear327@gmail.com>
2016-11-11 18:58:36 +0800

    Change back to original route

--- routes/web.php
+++ routes/web.php
@@ -55,7 +55,8 @@ Route::get('cards/{card}', 'CardsController@show');
 
 
 
-// Auth::routes();
+Auth::routes();
+/*
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login');
 Route::post('logout', 'Auth\LoginController@logout')->name('logout');
@@ -69,7 +70,7 @@ Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm'
 Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail');
 Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm');
 Route::post('password/reset', 'Auth\ResetPasswordController@reset');
-
+*/
 
 
 // controller -> think of a collection of route closures :)
