b17d8f8ba5919adcd173b0f193f899d8d46daffd
Henry Tseng <henrybear327@gmail.com>
2016-11-12 21:58:19 +0800

    Update homepage messages and default account type

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,16 +14,17 @@
                     @else student
                     @endif!<hr>
 
-                    大家所交上來的檔案！<br>
+                    以下是大家交上來的檔案，我稍微更改了一些HTML code以符合template engine的需求，並做了一些routing和database結合的測試。有功能的網頁，會有說明。要不然基本上網頁都是靜態的（按鈕都沒反應就是啦！)
+                    <br><br>
 
                     @if(Auth::user()->userType == 3)
-                        <a href="{{ url('/administrator/application') }}">管理員審查介面</a>
-                        <a href="{{ url('/administrator/accountManagement') }}">管理員帳號管理</a>
-                        <a href="{{ url('/administrator/capSetting') }}">管理員上限設定</a>
-                        <a href="{{ url('/administrator/statusSetting') }}">管理員系統狀態設定</a>
+                        <a href="{{ url('/administrator/application') }}">審查介面</a><br>
+                        <a href="{{ url('/administrator/accountManagement') }}">帳號管理</a>：可以觀看所有帳號和任意刪除帳號(對，刪除任意帳號）<br>
+                        <a href="{{ url('/administrator/capSetting') }}">金額上限設定</a><br>
+                        <a href="{{ url('/administrator/statusSetting') }}">系統狀態設定</a><br>
                     @elseif(Auth::user()->userType == 2) reviewer
                     @else
-                        <a href="{{ url('/studentApplicant/applicationForm') }}">學生申請表</a>
+                        <a href="{{ url('/studentApplicant/applicationForm') }}">學生申請表</a><br>
                     @endif
                 </div>
             </div>
